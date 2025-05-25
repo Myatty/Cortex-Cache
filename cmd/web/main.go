d69b5fa04b6136a9f9e53ce4202f1705b1827928
Myatty <myintmyatps664@gmail.com>
2025-05-25 22:20:38 +0630

    update(main.go): update main to use fileServer which serves ui static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,17 @@ import (
 func main() {
 
 	// ServeMux is a router which in this case register home function as handler for URL "/"
-	// URL "/" sub-tree pattern is a catch-all, all URL requests will be handled by this(its like "/**")
-
 	// create our own mux becoz DefaultServeMux is a global variable, any package can access it (Security Conerns)
 	mux := http.NewServeMux()
+
+	// create a file server which serves files out of the "./ui/static" directory
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// register the file server as the handler for all URL paths that start with "/static/"
+	// strips the "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// URL "/" sub-tree pattern is a catch-all, all URL requests will be handled by this(its like "/**")
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
