6dea76230ed129dfe7acf548e904058da24f4d56
Myatty <myintmyatps664@gmail.com>
2025-05-28 21:35:27 +0630

    refactor(comment)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -41,7 +41,7 @@ func main() {
 	// Initialize a new http.Server struct. We set the Addr and Handler fields so
 	// that the server uses the same network address and routes as before, and set
 	// the ErrorLog field so that the server now uses the custom errorLog logger in
-	// the event of any problems.
+	// the event of any problems
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
