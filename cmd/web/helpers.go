b79c7b6de24adc89b53d0a14c00df43cdb29bd5a
Myatty <myintmyatps664@gmail.com>
2025-10-03 15:27:20 +0630

    update(helpers.go): catch errors by writing templates to buffer first

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 )
 
-// The serverError helper writes an error message and stack trace to the errorLog,
-// then sends a generic 500 Internal Server Error response to the user
 func (app *application) serverError(w http.ResponseWriter, err error) {
 
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
@@ -17,15 +16,10 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 }
 
 // The clientError helper sends a specific status code and corresponding description
-// to the user. We'll use this later in the book to send responses like 400 "Bad
-// Request" when there's a problem with the request that the user sent.
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
-// For consistency, we'll also implement a notFound helper. This is simply a
-// convenience wrapper around clientError which sends a 404 Not Found response to
-// the user.
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
@@ -39,11 +33,14 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
 
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
