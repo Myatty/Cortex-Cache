dfb92e79822b371ebd93ddd968a5568e74c3317d
Myatty <myintmyatps664@gmail.com>
2025-10-03 16:24:15 +0630

    update(base.tmpl): to implement common dynamic data

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -9,8 +9,9 @@ import (
 
 // act as holding structure for dynamic data which will be passed to html tmpls
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
