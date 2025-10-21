266b58af0ee407f5efd92350fe81a88d862314a2
Myatty <myintmyatps664@gmail.com>
2025-10-21 20:58:11 +0630

    Update(snippetCreatePost): implement Form Data Validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,16 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
-
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"cortexcache.myatty.net/internal/models"
 	"github.com/julienschmidt/httprouter"
 )
 
-// Home handler function
-// http.ResponseWriter provides method for HTTP Response and sending it to user
 // *http.Request is pointer to struct which holds info about current request(HTTP method and URL being requested)
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
@@ -71,12 +70,14 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
+	// Parse the post form
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
+	// Get the input data
 	// aviod r.FormValue() and r.PostFormValue()
 	// becoz they silently ignore any errors returned by r.ParseForm()
 	title := r.PostForm.Get("title")
@@ -88,6 +89,30 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Validate the input data
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	// Dump errors(if exists) into HTTP response and return from Handler
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
+
+	// Insert into database
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
